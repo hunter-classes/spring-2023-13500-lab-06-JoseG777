4f3e9af951bc5e16cf7a0f87cff97571b4a5d029
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2023-03-10 00:20:02 -0500

    FIXED

--- decrypt.cpp
+++ decrypt.cpp
@@ -25,7 +25,7 @@ std::string decryptVigenere(std::string ciphertext, std::string keyword){
         if(j > keyword.length() - 1){ //if j is greater than the index of last letter, set it back to 0
             j = 0;
         }
-        if(isalpha(ciphertext[i]) == true){ // if the character is in the alphabet
+        if(isalpha(ciphertext[i])){ // if the character is in the alphabet
             plain1 += shiftChar(ciphertext[i], 26 - (keyword[j] - 97)); //shiftChar, index of speific character in chiper text
             j += 1;                                     //26 - (keyword[j] - 97), the keyword number will be at least 97 or at most 122
         }                                               //so 26 - (keyword[j] - 97) will give you a number you can use in shiftChar
